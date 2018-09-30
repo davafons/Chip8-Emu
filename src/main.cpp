fa99288eddbccf6077ecd466fbed5af3f6c9a98b
Dibad <dibadgithub@gmail.com>
2018-09-30 22:20:08 +0100

    Load Rom from command text

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <string>
+
 #include "chip8Facade.h"
 
-int main() {
-  Chip8Facade c8;
+int main(int argc, char *argv[]) {
+  std::string rom_path = "";
+  if (argc > 1) {
+    rom_path = argv[1];
+  }
 
+  Chip8Facade c8(rom_path);
+  c8.execute();
 
   return 0;
 }
